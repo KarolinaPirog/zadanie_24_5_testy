bd1895059a45375f8ddc1ac02da877783d4d7635
Karolina Piróg <karolina.swieca@wp.pl>
2018-08-15 13:16:46 +0200

    add calsulateStylePoints functions

--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,5 +1,12 @@
 const calculateStylePoints = (styleNotes) => {
-    
-  };
+  var maxNote = Math.max(styleNotes);
+  var minNote = Math.min(styleNotes);  
+  var sumNotes = 0;
+  for (let i=0; i<styleNotes.length; i++) {
+    sumNotes += styleNotes[i];
+  }
+  sumNotes -= (maxNote + minNote);
+  return sumNotes;
+};
   
-  module.exports = calculateStylePoints;
+module.exports = calculateStylePoints;
